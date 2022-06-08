4e94e475efd755c0a8b7c17ba67b8da7712af3df
kazeapp <104259498+kazeapp@users.noreply.github.com>
2022-06-08 06:09:20 +0545

    Update vzcloud.js

--- vzcloud.js
+++ vzcloud.js
@@ -1,8 +1,24 @@
 import fetch from "node-fetch";
-import fs from 'fs';
+import fs from "fs";
+
+// const fetch = require("node-fetch");
+// const fs = require("fs");
 
 // URL for data
 const URL = "https://vizcloud.cloud/embed/7ZPNN0M12P02";
+
+// function to get the raw data
+// const getRawData = (URL) => {
+//   return fetch(URL)
+//     .then((response) => {
+//       // console.log(response.text());
+//       return response.text();
+//     })
+//     .then((data) => {
+//       console.log(data);
+//       return data;
+//     });
+// };
 
 const getRawData = async (URL) => {
   const response = await fetch(URL, {
@@ -23,17 +39,32 @@ const getjsLink = async () => {
 
 const saveFile = async (data) => {
   // writeFile function with filename, content and callback function
-fs.writeFile('viz.js', data, function (err) {
-  if (err) throw err;
-  console.log('File is created successfully.');
-});
-}
+  fs.writeFile("viz.js", data, function (err) {
+    if (err) throw err;
+    console.log("File is created successfully.");
+  });
+};
 
 const getKeys = async () => {
   const jsLink = "https://vizcloud.cloud" + (await getjsLink());
   const vizcloudScriptData = await getRawData(jsLink);
   await saveFile(vizcloudScriptData);
+  await pushToGist();
   // console.log(vizcloudScriptData);
 };
 
 getKeys();
+
+const pushToGist = async () => {
+  var token = process.env.TOKEN;
+  var filename = "viz.js";
+  var gist_id = process.env.GIST;
+  fs.readFile(filename, "utf8", async (err, data) => {
+    var headers = { Authorization: `token ${token}` };
+    var r = await fetch("https://api.github.com/gists/" + gist_id, {
+      method: "PATCH",
+      body: JSON.stringify({ files: { "vizcloud.js": { content: data } } }),
+      headers: headers,
+    });
+  });
+};
